d554623c530404db33160fb58ef32794d4a3afd6
Bahadur <arzimbetov.bahadir@inbox.ru>
2023-11-12 10:57:15 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,17 +1,28 @@
 float problemSolution1(float consumed_water) {
     float cost;
     // write your code here
-    float fixed_amount = 13;
+
+//    if (consumed_water <= 30) {
+//        cost = 13 + consumed_water * 0.4;
+//    } else if (consumed_water <= 50) {
+//        cost = 13 + 30 * 0.4 + (consumed_water - 30) * 0.12;
+//    } else if (consumed_water <= 60) {
+//        cost = 13 + 30 * 0.4 + 20 * 0.12 + (consumed_water - 50) * 1.4;
+//    } else {
+//        cost = 13 + 30 * 0.4 + 20 * 0.12 + 10 * 1.4 + (consumed_water-60) * 1.5;
+//    }
 
     if (consumed_water <= 30) {
-        cost = fixed_amount + consumed_water * 0.4;
+        cost = 13 + consumed_water * 4.0 / 10.0;
     } else if (consumed_water <= 50) {
-        cost = fixed_amount + 30 * 0.4 + (consumed_water - 30) * 0.12;
+        cost = 13 + (30 * 4.0 / 10.0) + (consumed_water - 30) * 12.0 / 100.0;
     } else if (consumed_water <= 60) {
-        cost = fixed_amount + 30 * 0.4 + 20 * 0.12 + (consumed_water - 50) * 1.4;
+        cost = 13 + (30 * 4.0 / 10.0) + (20 * 12.0 / 100.0) + (consumed_water - 50) * 14.0 / 10.0;
     } else {
-        cost = fixed_amount + 30 * 0.4 + 20 * 0.12 + 10 * 1.4 + (consumed_water-60) * 1.5;
+        cost = 13 + (30 * 4.0 / 10.0) + (20 * 12.0 / 100.0) + (10 * 14.0 / 10.0) + (consumed_water - 60) * 15.0 / 10.0;
     }
+
+    std::cout<<cost;
 
     return cost;
 }
